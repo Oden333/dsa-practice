05a73653b955edebbec4e0450a10c36fdd046562
Ismail <galimov-ismail@mail.ru>
2026-03-04 13:34:36 +0300

    update Heap.go and Heap_test.go

--- structs/Heap_test.go
+++ structs/Heap_test.go
@@ -2,6 +2,7 @@ package structs
 
 import (
 	"container/heap"
+	"fmt"
 	"testing"
 )
 
@@ -39,5 +40,19 @@ func TestMinHeap_PushPopOrder(t *testing.T) {
 		if got != want {
 			t.Errorf("Pop() = %d, want %d", got, want)
 		}
+	}
+}
+
+func TestMinHeap_1(t *testing.T) {
+	// input := []int{5, 3, 1, 7, 2, 9}
+	// expected := []int{1, 2, 3, 5, 7, 9}
+
+	h := &MinHeap{}
+	for _, v := range []int{5, 3, 1, 7, 2} {
+		h.Push(v)
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(h.Pop()) // Должно вывести: 1 2 3 5 7
 	}
 }
